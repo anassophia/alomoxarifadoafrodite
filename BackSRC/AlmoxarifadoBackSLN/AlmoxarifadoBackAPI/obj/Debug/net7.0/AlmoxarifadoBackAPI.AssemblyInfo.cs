9d0bde7de81fa8b7010d2b68f22d5e232a1b2a25
Enzzo Samuel <144052012+anassophia@users.noreply.github.com>
2025-02-20 10:23:28 -0300

    a

--- BackSRC/AlmoxarifadoBackSLN/AlmoxarifadoBackAPI/obj/Debug/net7.0/AlmoxarifadoBackAPI.AssemblyInfo.cs
+++ BackSRC/AlmoxarifadoBackSLN/AlmoxarifadoBackAPI/obj/Debug/net7.0/AlmoxarifadoBackAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlmoxarifadoBackAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+678764290bd8ff9054c312ef39bd9fdc7c9ff7d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90a5ad00fa296e1ed310a48959134b7e9b572b1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlmoxarifadoBackAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlmoxarifadoBackAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
